3bdb955e3513913fc5d2ed9538ff35e966780cef
khalilelset <khoulioelset2000@gmail.com>
2024-09-03 13:07:41 +0300

    last changes before zaynab

--- modules/orders/order.services.ts
+++ modules/orders/order.services.ts
@@ -1,10 +1,10 @@
 import { createConnection } from "../../config/db";
 import { FieldPacket } from "mysql2";
 import { Order, User } from "./types/order.interface";
-import axios from 'axios';
+import axios from "axios";
 export const getAllOrders = async () => {
+  const connection = await createConnection();
   try {
-    const connection = await createConnection();
     const [orders]: [Order[], FieldPacket[]] = await connection.query(`
       SELECT * 
       FROM POSystemdb.orders 
@@ -27,6 +27,10 @@ export const getAllOrders = async () => {
   } catch (error) {
     console.error("Error retrieving orders:", error);
     throw new Error("Error retrieving orders");
+  }finally {
+    if (connection) {
+      await connection.end();
+    }
   }
 };
 
@@ -40,7 +44,6 @@ export const getOrderById = async (workerId: string) => {
   return orders.length > 0 ? orders : [];
 };
 
-
 export const createOrder = async (
   orderId: string,
   worker_id: string,
@@ -52,23 +55,23 @@ export const createOrder = async (
 ) => {
   const connection = await createConnection();
   try {
-       const connection = await createConnection();
+    const connection = await createConnection();
     console.log("Received order creation request with the details ");
     const unitPriceNumber = parseFloat(unit_price);
     if (isNaN(unitPriceNumber)) {
       throw new Error("Invalid unit_price format");
     }
     console.log(`Converted unit_price to number: ${unitPriceNumber}`);
- 
+
     const []: [any, FieldPacket[]] = await connection.query(
       `
       INSERT INTO POSystemdb.orders (ID,worker_id,
         order_name, order_desc, link, quantity, unit_price,order_status
       ) VALUES (?,?, ?, ?, ?, ?, ? ,"inprogress")
     `,
-      [orderId,worker_id, order_name, order_desc, link, quantity, unit_price]
+      [orderId, worker_id, order_name, order_desc, link, quantity, unit_price]
     );
-    
+
     return {
       message: "order added sucessfully",
     };
@@ -79,8 +82,7 @@ export const createOrder = async (
       console.error("Unknown error occurred");
     }
     throw new Error("Error creating order");
-  }
-  finally {
+  } finally {
     if (connection) {
       await connection.end();
     }
@@ -97,14 +99,16 @@ export const AIProcessing = async (
     const apiUrl =
       "http://last-1199026659.us-west-2.elb.amazonaws.com/product/cv";
 
-    const response = await axios.post(apiUrl, {
-      url,
-      description,
-      price,
-    }).catch((error) => {
-      console.error("Error in Axios request:", error.message);
-      throw new Error("Failed to get a response from AI API");
-    });
+    const response = await axios
+      .post(apiUrl, {
+        url,
+        description,
+        price,
+      })
+      .catch((error) => {
+        console.error("Error in Axios request:", error.message);
+        throw new Error("Failed to get a response from AI API");
+      });
 
     if (!response.data.result) {
       throw new Error("no result from AI API");
@@ -114,7 +118,7 @@ export const AIProcessing = async (
       console.error("Error creating database connection:", error.message);
       throw new Error("Failed to connect to the database");
     });
-//console.log("balash tabe3 3al dab")
+    //console.log("balash tabe3 3al dab")
     try {
       const query = `
         UPDATE orders
@@ -126,7 +130,7 @@ export const AIProcessing = async (
         response.data.result.analysis,
         id,
       ]);
-    } catch (error :any) {
+    } catch (error: any) {
       console.error("Error executing query:", error.message);
       throw new Error("Failed to update the order");
     } finally {
@@ -134,11 +138,9 @@ export const AIProcessing = async (
         await connection.end();
       }
     }
-
-  } catch (error : any) {
+  } catch (error: any) {
     console.error("Error in AIProcessing:", error.message);
     throw new Error(error.message);
   }
+  
 };
-
-
